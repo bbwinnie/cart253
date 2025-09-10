f59b116883fa4ceef14da6b2f9d6654a00a5de77
bbwinnie <nini200702@gmail.com>
2025-09-10 19:58:34 -0400

    A cloud
    fix the code for land and add a cloud

--- topics/class-challenge/landscape-challenge/js/script.js
+++ topics/class-challenge/landscape-challenge/js/script.js
@@ -27,19 +27,38 @@ function setup() {
 function draw() {
 
     land()
+    cloud()
 
 }
 
 function land() {
 
-    // set the land as a ellipse, add the color and stroke
-    ellipse(150, 500, 450, 300);
+    // set the land as a ellipse, add the color and stroke    
+    push();
     fill("#007ca5");
     noStroke();
+    ellipse(150, 500, 450, 300);
+    pop();
 
     // set the secound land as a elllipse, add the color and stroke
-    ellipse(430, 550, 500, 300);
+    push();
     fill("#157291");
     noStroke();
-
+    ellipse(430, 550, 500, 300);
+    pop();
 }
+
+function cloud() {
+
+    // A cloud from pippin however we change the color and location
+    push();
+    noStroke();
+    fill("#c7c7c7");
+    ellipse(400, 100, 100, 100);
+    ellipse(480, 80, 100, 100);
+    ellipse(460, 120, 60, 60);
+    ellipse(490, 130, 60, 60);
+    ellipse(520, 120, 60, 60);
+    pop();
+}
+
