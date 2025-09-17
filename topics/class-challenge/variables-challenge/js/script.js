ea297181c2cf2273cc458069da590c8db1c8c6d6
bbwinnie <nini200702@gmail.com>
2025-09-17 19:30:27 -0400

    started the variables challenge

---
+++ topics/class-challenge/variables-challenge/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Mr.Furious Furious
+ * weini wang
+ * 
+ * working with ziyan pan
+ * 
+ * variables Challenge
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
