1a818bce88403418c2ffc89f9062be152721f093
bbwinnie <nini200702@gmail.com>
2025-09-24 19:05:52 -0400

    conditionals challenge

---
+++ topics/class-challenge/conditionals-challenge/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * conditionals challenge
+ * weini wang
+ * 
+ * working with ziyan pan
+ * 
+ * class challenge for the conditionals, distance
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
