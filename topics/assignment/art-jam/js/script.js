30a646bb7666a86d382963dbb403347ebd82562d
bbwinnie <nini200702@gmail.com>
2025-09-26 22:14:58 -0400

    Add the  user interaction for the frog cheek line and testing the stareyes

--- topics/assignment/art-jam/js/script.js
+++ topics/assignment/art-jam/js/script.js
@@ -69,7 +69,10 @@ let frogCheekLine = {
     y1: 120,
     leftLx2: 245,
     rightlx2: 345,
-    y2: 135
+    y2: 135,
+    x: 320,
+    y: 240,
+    strokeColor: 0
 }
 
 
@@ -97,6 +100,8 @@ function draw() {
 
     //DRAW THE FROGHEAD
     frogHead();
+
+    stareyes();
 
 }
 
@@ -156,8 +161,19 @@ function frogHead() {
     //SET THE CHEEK DETAIL FOR THE FROG, MAKE THE COLOR CHANGE BY THE MOUSE
     push();
     strokeWeight(frogCheekLine.strokeWeight);
-    //MAKE THE FROGCHEEKLINE COLOR CHANGED, R BY MOUSEX, G BY MOUSEY AND B WILL BE A RANDOM NUMBER BETWEEN 0-255.
-    stroke(mouseX, mouseY, random(0, 255));
+
+    //MAKE THE FROGCHEEKLINE COLOR CHANGED BY MOUSE AND RANDOM B. WHEN THE MOUSE NOT MOVE, THE RGB(B) WILL NOT CHANGE. IF MOUSE MOVE, THE RGB(B)COLOR WILL CHANGE.
+    if (frogCheekLine.x == mouseX && frogCheekLine.y == mouseY) {
+        stroke(mouseX, mouseY, frogCheekLine.strokeColor);
+    }
+
+    else {
+        frogCheekLine.strokeColor = random(0, 255);
+        stroke(mouseX, mouseY, frogCheekLine.strokeColor);
+        frogCheekLine.x = mouseX;
+        frogCheekLine.y = mouseY;
+    }
+
     line(frogCheekLine.leftLX1, frogCheekLine.y1, frogCheekLine.leftLx2, frogCheekLine.y2);
     line(frogCheekLine.leftLX1 - 20, frogCheekLine.y1, frogCheekLine.leftLx2 - 20, frogCheekLine.y2);
     line(frogCheekLine.leftLX1 - 40, frogCheekLine.y1, frogCheekLine.leftLx2 - 40, frogCheekLine.y2);
@@ -167,3 +183,82 @@ function frogHead() {
     pop();
 
 }
+
+
+
+
+// function stareyes() {
+
+//     push();
+
+//     stareyesP.rectH += stareyesP.increase
+//     noStroke();
+//     fill("#f5f821ff");
+//     rect(350, 50, 20, stareyesP.rectH,);
+//     pop();
+
+//     push();
+//     noStroke();
+//     fill("#000000ff");
+//     scale(1, 1);
+//     arc(350, 50, 20, 30, 0, 90);
+//     arc(370, 50, 20, 30, 90, 180);
+//     arc(350, 80, 20, 30, 270, 360);
+//     arc(370, 80, 20, 30, 180, 270);
+//     pop();
+
+
+// }
+
+
+let stareyesP = {
+    quandx1and3: 240,
+    quandy1and2: 60,
+    quandx2and4: 260,
+    quandy3and4: 90,
+    arc1x: 240,
+    arc2x: 260,
+    arc1y: 60,
+    arc2y: 90,
+    arcw: 20,
+    arch: 30,
+    increase: 0.1,
+    increaseArc: 0.2
+}
+
+function stareyes() {
+
+    console.log(stareyesP.arch);
+    push();
+    fill("#000000ff");
+    stroke("#ffffffff");
+    strokeWeight(1);
+    ellipse(250, 75, 40, 50);
+    pop();
+
+    push();
+    stareyesP.quandy1and2 -= stareyesP.increase
+    stareyesP.quandy3and4 += stareyesP.increase
+    stareyesP.quandy1and2 = constrain(stareyesP.quandy1and2, 60, 62);
+    stareyesP.quandy3and4 = constrain(stareyesP.quandy3and4, 90, 92);
+    noStroke();
+    fill("#f5f821ff");
+    quad(240, stareyesP.quandy1and2, 260, stareyesP.quandy1and2, 260, stareyesP.quandy3and4, 240, stareyesP.quandy3and4);
+    pop();
+
+    push();
+    noStroke();
+    fill("#000000ff");
+    stareyesP.arc1y -= stareyesP.increase
+    stareyesP.arc2y += stareyesP.increase
+    stareyesP.arch += stareyesP.increaseArc
+    stareyesP.arch = constrain(stareyesP.arch, 30, 40);
+    stareyesP.arc2y = constrain(stareyesP.arc2y, 90, 95);
+    stareyesP.arc1y = constrain(stareyesP.arc1y, 60, 65);
+    arc(240, stareyesP.arc1y, 20, stareyesP.arch, 0, 90);
+    arc(260, stareyesP.arc1y, 20, stareyesP.arch, 90, 180);
+    arc(240, stareyesP.arc2y, 20, stareyesP.arch, 270, 360);
+    arc(260, stareyesP.arc2y, 20, stareyesP.arch, 180, 270);
+    pop();
+}
+
