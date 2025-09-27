0fa6513c3d68048387ee0975abda449e61ac9fb1
bbwinnie <nini200702@gmail.com>
2025-09-26 20:07:03 -0400

    add the frogmouth and frogcheek
    make the frog cheek line changed by mouse x, mouse y and a random number.

--- topics/assignment/art-jam/js/script.js
+++ topics/assignment/art-jam/js/script.js
@@ -28,6 +28,7 @@ let frogE = {
     arcStopP: 0
 }
 
+//SET THE DETAIL FOR FROGEYES
 let frogEyes = {
     fill: "#000000ff",
     y: 72,
@@ -38,6 +39,40 @@ let frogEyes = {
     strokeWeight: 2.5,
     strokeColor: "#ffffffff"
 }
+
+//SET THE DETAIL FOR FROGMOUTH
+let frogMouth = {
+    fill: "#feffafff",
+    x: 300,
+    y: 100,
+    w: 30,
+    h: 150,
+    arcStartP: 0,
+    arcStopP: 180
+}
+
+//SET THE DETAIL FOR FROGCHEEK
+let frogCheek = {
+    fill: "#eecbcbff",
+    leftX: 230,
+    rightX: 370,
+    y: 130,
+    w: 70,
+    h: 50,
+}
+
+//SET THE LINE INSIDE THE FROGCHEECK
+let frogCheekLine = {
+    strokeWeight: 3,
+    leftLX1: 255,
+    RightLX1: 355,
+    y1: 120,
+    leftLx2: 245,
+    rightlx2: 345,
+    y2: 135
+}
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
@@ -62,6 +97,7 @@ function draw() {
 
     //DRAW THE FROGHEAD
     frogHead();
+
 }
 
 //DISPLAY THE FORGHEAD
@@ -100,6 +136,34 @@ function frogHead() {
     translate(frogE.rightEarX, frogEyes.y);
     rotate(frogEyes.rotateRight);
     ellipse(0, 0, frogEyes.w, frogEyes.h);
+    pop();
+
+    //SET THE FROG MOUTH 
+    push();
+    fill(frogMouth.fill);
+    noStroke();
+    arc(frogMouth.x, frogMouth.y, frogMouth.w, frogMouth.h, frogMouth.arcStartP, frogMouth.arcStopP, CHORD);
+    pop();
+
+    //SET THE CHEEK FOR THE FROG
+    push();
+    fill(frogCheek.fill);
+    noStroke();
+    ellipse(frogCheek.leftX, frogCheek.y, frogCheek.w, frogCheek.h);
+    ellipse(frogCheek.rightX, frogCheek.y, frogCheek.w, frogCheek.h);
+    pop();
+
+    //SET THE CHEEK DETAIL FOR THE FROG, MAKE THE COLOR CHANGE BY THE MOUSE
+    push();
+    strokeWeight(frogCheekLine.strokeWeight);
+    //MAKE THE FROGCHEEKLINE COLOR CHANGED, R BY MOUSEX, G BY MOUSEY AND B WILL BE A RANDOM NUMBER BETWEEN 0-255.
+    stroke(mouseX, mouseY, random(0, 255));
+    line(frogCheekLine.leftLX1, frogCheekLine.y1, frogCheekLine.leftLx2, frogCheekLine.y2);
+    line(frogCheekLine.leftLX1 - 20, frogCheekLine.y1, frogCheekLine.leftLx2 - 20, frogCheekLine.y2);
+    line(frogCheekLine.leftLX1 - 40, frogCheekLine.y1, frogCheekLine.leftLx2 - 40, frogCheekLine.y2);
+    line(frogCheekLine.RightLX1, frogCheekLine.y1, frogCheekLine.rightlx2, frogCheekLine.y2);
+    line(frogCheekLine.RightLX1 + 20, frogCheekLine.y1, frogCheekLine.rightlx2 + 20, frogCheekLine.y2);
+    line(frogCheekLine.RightLX1 + 40, frogCheekLine.y1, frogCheekLine.rightlx2 + 40, frogCheekLine.y2);
     pop();
 
 }
