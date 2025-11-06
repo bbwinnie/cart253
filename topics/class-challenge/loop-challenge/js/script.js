2ce17873e0920f43ee1503e6362dbd7d601bdde2
bbwinnie <nini200702@gmail.com>
2025-11-05 20:02:21 -0500

    step 2 and 3 did the while loop for the line

--- topics/class-challenge/loop-challenge/js/script.js
+++ topics/class-challenge/loop-challenge/js/script.js
@@ -1,24 +1,81 @@
 /**
- * Title of Project
- * Author Name
+ * loop challenge
+ * weini wang 
+ * working with ziyan pan
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A series of lines across the canvas
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas
+ */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws lines across the canvas with increasing thickness and
+ * gradually lightening colour
+ */
 function draw() {
+    background("pink");
 
+    //defaut the x, color and y.
+    let x = 0;
+    let c = 0;
+    let y = 0;
+    let b = 0;
+
+    // While x smaller than width, draw a line.
+    while (x < width) {
+        stroke(c);
+        line(x, 0, x, height);
+        c += 50;
+        x += 50;
+    }
+
+    // While y smaller than Height, draw a line.
+    while (y < height) {
+        stroke(b);
+        line(0, y, width, y);
+        b += 50;
+        y += 50;
+    }
+
+    for
+
+    // stroke(0);
+    // line(0, 0, 0, height);
+
+    // stroke(25);
+    // line(50, 0, 50, height);
+
+    // stroke(50);
+    // line(100, 0, 100, height);
+
+    // stroke(75);
+    // line(150, 0, 150, height);
+
+    // stroke(100);
+    // line(200, 0, 200, height);
+
+    // stroke(125);
+    // line(250, 0, 250, height);
+
+    // stroke(150);
+    // line(300, 0, 300, height);
+
+    // stroke(175);
+    // line(350, 0, 350, height);
+
+    // stroke(200);
+    // line(400, 0, 400, height);
+
+    // stroke(225);
+    // line(450, 0, 450, height);
+
+    // stroke(250);
+    // line(500, 0, 500, height);
 }
