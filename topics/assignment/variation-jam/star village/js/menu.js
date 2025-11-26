ee05cea94e610c11bf48045fd8e9fac299764144
bbwinnie <nini200702@gmail.com>
2025-11-26 16:14:31 -0500

    updated the image file and set the mean pages

--- topics/assignment/variation-jam/star village/js/menu.js
+++ topics/assignment/variation-jam/star village/js/menu.js
@@ -4,18 +4,37 @@
  * This keeps the stuff the menu needs to do *separate* from the rest of the program.
  */
 
-const menuText = `
-Star village`
+// set the game name 
+const menuText = "Star Village";
+
+//define my font and image variable
+let myFont;
+let bgImg;
+let textBg;
+let game1BG;
+let game2BG;
+let game3BG;
+
+// perload all the fonts and images
+function preload() {
+    myFont = loadFont('assets/font/PixelifySans-VariableFont_wght.ttf');
+    bgImg = loadImage('assets/images/startbackground.jpeg');
+    textBg = loadImage('assets/images/pages.gif');
+    game1BG = loadImage('assets/images/starb.png');
+    game2BG = loadImage('assets/images/starr.png');
+    game3BG = loadImage('assets/images/stary.png');
+    preloadGame1();
+}
 
 /**
  * Display the main menu
  */
 function menuDraw() {
-    background(0);
-
     push();
-    fill(255);
+    background(bgImg);
+    textFont(myFont);
     textSize(32);
+    fill("#ffcc00")
     textAlign(CENTER, CENTER);
     text(menuText, width / 2, height / 2);
     pop();
